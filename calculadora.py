b525684f98c04424ee15c702f82e836e1c3a0b41
olliras <samuelreynaldo601@gmail.com>
2023-09-11 12:44:56 -0500

    mi primer programa de calculadora

---
+++ calculadora.py
@@ -0,0 +1,23 @@
+'''
+Autor: Samuel Reynaldo olvera lira
+fecha: 11 septiembre
+Descripcion: Calculadora que sume
+'''
+
+a = input("Ingresa el primer valor")
+b = input("Ingresa el segundo valor")
+print(type(a), type(b))
+print ("suma" , a + b)
+a = float(a)
+b = float(b)
+print(type(a), type(b))
+print ("suma" , a + b)
+
+print(type(a), type(b))
+print ("resta" , a - b)
+
+print(type(a), type(b))
+print ("division" , a / b)
+
+print(type(a), type(b))
+print ("suma" , a * b)
