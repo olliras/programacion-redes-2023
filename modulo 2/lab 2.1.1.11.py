0aadc8daae139685545724985718b23bc2a8c9ce
olliras <142609705+olliras@users.noreply.github.com>
2023-10-05 18:09:42 -0600

    Delete modulo 2 directory

--- modulo 2/lab 2.1.1.11.py
+++
@@ -1,8 +0,0 @@
-'''
-nombre: Samuel reynaldo olvera lira
-fecha: 26 de septiembre 2023
-descripcion: laborartorios modulo 2
-'''
-print('''Estoy
-""aprendiendo""
-"""Python"""''')
--- modulo 2/lab 2.1.1.18.py
+++
@@ -1,7 +0,0 @@
-'''
-nombre: Samuel reynaldo olvera lira
-fecha: 26 de septiembre 2023
-descripcion: laborartorios modulo 2
-'''
-print("Fundamentos", "Programación", "en...Python", sep="***", end="")
-print()
