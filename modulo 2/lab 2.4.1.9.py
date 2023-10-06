d59e3d1169056a68ee794bd507007f6da1fa5041
olliras <142609705+olliras@users.noreply.github.com>
2023-10-05 18:07:34 -0600

    Add files via upload

---
+++ modulo 2/lab 2.4.1.9.py
@@ -0,0 +1,13 @@
+'''
+nombre: Samuel reynaldo olvera lira
+fecha: 26 de septiembre 2023
+descripcion: laborartorios modulo 2
+'''
+millas = 9.75
+kilometros = 15.68
+
+millas_a_kilometros = millas * 1.60934
+kilometros_a_millas = kilometros / 1.60934
+
+print(millas, "millas son", round(millas_a_kilometros, 2), "kilómetros")
+print(kilometros, "kilómetros son", round(kilometros_a_millas, 2), "millas")
